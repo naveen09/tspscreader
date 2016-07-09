87a5832970bf2c74f639550ea5ee4be738daeb29
naveen09 <aechannaveen@gmail.com>
2016-07-09 17:23:22 +0530

    notifications and keys code

--- tspsc.js
+++ tspsc.js
@@ -2,55 +2,108 @@ var fs = require('fs');
 var webdriver = require('selenium-webdriver');
 var By = webdriver.By;
 var until = webdriver.until;
-var driver = new webdriver.Builder()
-    .forBrowser('firefox')
-    .build();
+var driver = new webdriver.Builder().forBrowser('firefox').build();
 var jsonData;
-
-driver.get('http://tspsc.gov.in');
-driver.getTitle().then(function(title){
-  log("TSPSC Page loaded...")
-  log(title);
-  log("--------- Looking for notifications container...--------");
+var FILE_PATH;
+var WEBSITE;
+fs.readFile('props.txt', 'utf8', function(err, props) {
+    if (err) throw err;
+    var propsData = JSON.parse(props);
+    WEBSITE = propsData["WEBSITE"];
+    FILE_PATH = propsData["FILE_PATH"];
+    log('Website: '+WEBSITE);
+    log('File Path: '+FILE_PATH);
+    init();
 });
 
-fs.readFile('tspsc-export.json', 'utf8', function (err, data) {
-  if (err) throw err;
-  jsonData = JSON.parse(data);
-  jsonData['news'] = "";
-  log('################################################')
-});
-
-driver.findElement(By.className('arrow-list')).then(readNotificationsContainer);
-
-function readNotificationsContainer(container){
-  container.findElements(By.tagName('li')).then(readNotificationsList);
-}
-
-function readNotificationsList(list){
-  var newsJson = "";
-  var index = 0;
-  list.forEach(function(li){
-    li.findElement(By.tagName('a')).then(function(a){
-      a.getAttribute('href').then(function(ref){
-        a.getText().then(function(text){
-          var txt = text.replace(/\//g,'-').replace(/\./g,"");
-          newsJson += "\""+(++index)+"::"+txt+"\":{\"url\":\""+ref+"\"}"
-          if(index == list.length){
-            log(newsJson);
-          }else{
-            newsJson+=",";
-          }
-        })
-      });
+function init() {
+    driver.get(WEBSITE);
+    driver.getTitle().then(checkPageLoad);
+    fs.readFile(FILE_PATH, 'utf8', function(err, data) {
+        if (err) throw err;
+        jsonData = JSON.parse(data);
+        jsonData['news'] = {};
+        jsonData['keys'] = {};
     });
-  });
 }
 
-// driver.findElement(By.name('btnG')).click();
-// driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-driver.quit();
+function checkPageLoad(title) {
+    log("Website loaded...")
+    log(title);
+    log("--------- Looking for notifications container ---------");
+    readNotifications();
+}
 
-function log(text){
-  console.log(text);
+function readNotifications() {
+    driver.findElement(By.className('arrow-list')).then(readNotificationsContainer);
+}
+
+function readNotificationsContainer(container) {
+    container.findElements(By.tagName('li')).then(readNotificationsList);
+}
+
+function readNotificationsList(list) {
+    var index = list.length;
+    list.forEach(function(li) {
+        li.findElement(By.tagName('a')).then(function(a) {
+            a.getAttribute('href').then(function(ref) {
+                a.getText().then(function(text) {
+                    var obj = {};
+                    var txt = format(text);
+                    var urlObj = {
+                        "url": ""
+                    };
+                    urlObj.url = ref;
+                    var key = index-- + "::" + txt;
+                    jsonData.news[key] = urlObj;
+                    if (index == 0) {
+                        log('---------  Completed Notifications Section ---------');
+                        readKeys();
+                    }
+                })
+            });
+        });
+    });
+}
+
+function readKeys() {
+    log('--------- Reading Keys Section --------- ');
+    driver.findElement(By.className('boxxx blue')).then(readKeysContainer);
+}
+
+function readKeysContainer(container) {
+    container.findElements(By.tagName('h5')).then(readKeysList);
+}
+
+function readKeysList(list) {
+    var index = list.length;
+    list.forEach(function(li) {
+        li.findElement(By.tagName('a')).then(function(a) {
+            a.getAttribute('href').then(function(ref) {
+                a.getText().then(function(text) {
+                    var obj = {};
+                    var txt = format(text);
+                    var url = ref;
+                    var key = index-- + "::" + txt;
+                    jsonData.keys[key] = url;
+                    if (index == 0) {
+                        fs.writeFile('tspsc-export.json', JSON.stringify(jsonData), function(err) {
+                            if (err) throw err;
+                            log('---------  Completed Keys Section ---------');
+                            log('--------- Keys content writen to file ---------');
+                            driver.quit();
+                        });
+                    }
+                })
+            });
+        });
+    });
+}
+
+function log(text) {
+    console.log(text);
+}
+
+function format(text) {
+    return text.replace(/\//g, '-').replace(/\./g, "").replace(/\\n/g,'');
 }
